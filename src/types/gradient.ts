c6b429e610cbc63ba25de792aacdf3e664a4bab3
Ivan Buryak <ivan.buryak@gmail.com>
2025-04-17 17:05:01 +0500

    Add support for numeric direction (angle) for  option

--- src/types/gradient.ts
+++ src/types/gradient.ts
@@ -5,13 +5,14 @@
  *
  * @param {number} opacity - specifies gradient opacity. When set to 0, gradient is not applied. Value range: `0` - `1`.
  * @param {string} [color="000"] - (optional) specifies gradient color. If omitted, the gradient is black. Default `"000"`.
- * @param {"down" | "up" | "right" | "left"} [direction="down"] - (optional) specifies gradient direction. Default `"down"`.
+ * @param {"down" | "up" | "right" | "left" | number} [direction="down"] - (optional) specifies gradient direction. Default `"down"`.
  *
  * Available values:
  * - `down` - the top side of the gradient is transparrent, the bottom side is opaque
  * - `up` - the bottom side of the gradient is transparrent, the top side is opaque
  * - `right` -  the left side of the gradient is transparrent, the right side is opaque
  * - `left` - the right side of the gradient is transparrent, the left side is opaque
+ * - `number` - angle in degrees (clockwise). `0` creates a gradient from top to bottom; `90` creates a gradient from right to left.
  * @param {number} [start=0.0] - (optional) specifies the start point of the gradient. Value range: `0.0` - `1.0`. Default `0.0`.
  * @param {number} [stop=1.0] - (optional) specifies the end point of the gradient. Value range: `0.0` - `1.0`. Default `1.0`.
  *
@@ -25,12 +26,15 @@
  * // Apply a gray gradient with 0.3 opacity from the left to the right of the image with the start point at 0.2 and the end point at 0.8
  * {gradient: {opacity: 0.3, direction: "right", color: "808080", start: 0.2, stop: 0.8}}
  *
+ * // Apply a gray gradient with 0.3 opacity at a 45 degree angle
+ * {gradient: {opacity: 0.3, direction: 45, color: "808080"}}
+ *
  * @see https://docs.imgproxy.net/generating_the_url?id=gradient
  */
 interface Gradient {
   opacity: number;
   color?: string;
-  direction?: "down" | "up" | "right" | "left";
+  direction?: "down" | "up" | "right" | "left" | number;
   start?: number;
   stop?: number;
 }
